f6437ffbb8320aef91a92fc9a130cdd0c03ccbb2
mintleaf <mintchipleaf@gmail.com>
2014-08-02 21:54:25 -0400

    First (late) commit

---
+++ Assets/Scripts/PaddleMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMove : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(0,10);
+	
+	private Vector2 movement;
+	
+	void Update () {
+		float inputX = 0;
+		float inputY = 0;
+		
+		if (Input.GetKey ("up")){
+			inputY = 1;
+		}
+		if (Input.GetKey ("down")){
+			inputY = -1;
+		}
+		
+		
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+	void FixedUpdate () {
+		rigidbody2D.velocity = movement;
+		//rigidbody2D.AddForce(Vector2.up);
+	}
+
+}
---
+++ Assets/Scripts/SpawnAssets.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnAssets : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}	
+}
